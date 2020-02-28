5fc9420419db1cdbdfc392184c10e5ff40cec1be
EskerOn <35613070+EskerOn@users.noreply.github.com>
2020-02-28 16:42:00 -0600

    Add files via upload
    Philosopher Class added

---
+++ filosofo.py
@@ -0,0 +1,41 @@
+#H=hungry, E=eating, T=thinking
+import threading
+import time
+class Filosofo(threading.Thread):
+    def __init__(self, lock):
+        threading.Thread.__init__(self)
+        self.lock=lock
+        self.left=None
+        self.right=None
+        self.status="T"
+    def setLeft(self,fil):
+        self.left=fil
+    def setRight(self,fil):
+        self.right=fil
+    def spendTime(self):
+        time.sleep(5)
+    def taste(self):
+        if(self.status== "H" and self.left.status!="E" and self.right.status!="E"):
+            self.lock.acquire()
+            self.status="E"
+            self.lock.release()
+    def takeFork(self):
+        self.lock.acquire()
+        self.status="H"
+        self.taste()
+        self.lock.release()
+    def leaveFork(self):
+        self.lock.acquire()
+        self.status="T"
+        self.left.taste()
+        self.right.taste()
+        self.lock.release()
+    def eat(self):        
+        self.spendTime()
+    def think(self):
+        self.spendTime()
+    def run(self):
+        self.think()
+        self.takeFork()
+        self.eat()
+        self.leaveFork()
