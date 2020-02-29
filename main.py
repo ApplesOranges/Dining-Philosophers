84e8f260021b7e56ff036df422c4e80580371778
ApplesOranges <david.r.arancibia@gmail.com>
2020-02-28 23:49:57 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,28 @@ from filosofo import Filosofo
 lock = threading.RLock()
 Filosofos = [Filosofo(lock) for i in range(5)]
 i=0
+thrds=[]
+states = []
+
+
+def showStates():
+    j = 0
+    for fil in Filosofos:
+        states[j] = fil.status
+    print(states)
+
+
 for fil in Filosofos:
-    fil.setLeft=Filosofos[i-1]
-    fil.setRight=Filosofos[i+1]
+    if (i == 4):
+        fil.setRight(Filosofos[0])
+    else:
+        fil.setRight(Filosofos[i+1])
+    fil.setLeft(Filosofos[i-1])
+    i += 1
+i = 0
+
+for fil in Filosofos:
+    thrds[i] = threading.Thread(fil.run())
+    thrds[i].start()
     i+=1
+    showStates()
